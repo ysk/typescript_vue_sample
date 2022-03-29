0f36c2eb1f4e56b4863fc5f69f9d63aab33a039d
Yusuke Ikeda <yusuke_aono@yahoo.co.jp>
2022-03-29 22:30:33 +0900

    Vue Routerの実装

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="navigation-bar">
-    ナビゲーションバーです
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="/main">TechpitNotion</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/main"><i class="fas fa-file-alt"></i> ノートリスト</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown">
+            <i class="fas fa-user-graduate"></i> 学習用ページ
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/study/binding">学習1</a></li>
+            <li><a class="dropdown-item" href="/study/directive">学習2</a></li>
+            <li><a class="dropdown-item" href="/study/event">学習3</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -12,7 +32,5 @@ export default {
 <style scoped>
 .navigation-bar {
   height: 60px;
-  color: white;
-  background-color: #6c757d;
 }
 </style>
